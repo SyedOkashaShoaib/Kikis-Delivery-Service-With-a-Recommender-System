b237689c32678b64e303216939eb7788fd5307e8
Kumail Ali <rocksalot50@gmail.com>
2025-11-22 21:17:48 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,72 +1,13 @@
 from . import db, login
 from sqlalchemy.sql import func
+import sqlalchemy.orm as so
 import enum
-from sqlalchemy import Enum
+from sqlalchemy import Enum, PrimaryKeyConstraint, Time
 from datetime import date
 from flask_login import UserMixin
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
-
-class Customer_Profile(db.Model, UserMixin):
-    __tablename__ = 'customer_profile'
-    username = db.Column(db.String(30), primary_key=True)
-    # user_password = db.Column(db.String(255), nullable=False) #remove this
-    user_phone = db.Column(db.String(11), nullable=True)
-    user_email = db.Column(db.String(30), nullable=True)
-    user_reg_date=db.Column(db.Date, nullable=False, default=date.today)
-
-
-    def __rep__(self):
-        return f"<Customer : {self.username} > " #remove this
-    
-    # def set_password(self, password):
-    #     self.user_password = generate_password_hash(password)  #remove this
-    # def check_password(self, password):
-    #     return check_password_hash(self.user_password, password) #remove this..
-
-    def get_id(self):
-        return self.username   #i have overridden the usermixin function,for now ...
-
-
-class Vendor_Profile(UserMixin, db.Model):
-    __tablename__='vendor_profile'
-    v_username = db.Column(db.String(30), primary_key=True)
-    v_phone = db.Column(db.String(11), nullable=False)
-    v_email = db.Column(db.String(30), nullable=False)
-    v_reg_date = db.Column(db.Date, nullable=False)  #How to add a date field in the form?
-    v_verified = db.Column(db.Boolean, nullable=False, default=True)
-    #  vendorusername VARCHAR(50) PRIMARY KEY,
-    # -- vendor_password VARCHAR(8) NOT NULL,
-    # vendor_phone VARCHAR(11),
-    # vendor_email VARCHAR(30),
-    # vendor_reg_date DATE NOT NULL,
-    # vendor_verified BOOLEAN DEFAULT TRUE NOT NULL,
-    # CONSTRAINT verified_check CHECK (vendor_verified IN (TRUE, FALSE))
-
-
-
-class Delivery_Profile(UserMixin, db.Model):
-    __tablename__='it_profile'
-    d_username= db.Column(db.String(30), primary_key=True)
-    d_phone = db.Column(db.String(11), nullable=False)
-    d_email = db.Column(db.String(30), nullable=False)
-    d_reg_date = db.Column(db.Date, nullable=False)
-    d_last_paid=db.Column(db.Date, nullable=True)
-    d_license_stat = db.Column( db.Boolean, nullable=False, default=True) #change this to db.checkconstraint 
-#     CREATE TABLE it_profile (
-#     delivery_username VARCHAR(30) PRIMARY KEY,
-#     delivery_password VARCHAR(8) NOT NULL,
-#     delivery_phone VARCHAR(11),
-#     delivery_email VARCHAR(30),
-#     delivery_reg_date DATE NOT NULL,
-#     last_paid_date DATE,
-#     license_status VARCHAR(10) NOT NULL CHECK (license_status IN ('EXPIRED', 'VALID')),
-#     expiry_date DATE NOT NULL
-# );
-
-
-
+'''
 class Registered_Users(UserMixin, db.Model):
     __tablename__="registered_users"
     register_id = db.Column(db.Integer, primary_key=True)
@@ -78,32 +19,144 @@ class Registered_Users(UserMixin, db.Model):
         db.CheckConstraint("userrole IN ('ADMIN','CUSTOMER','VENDOR','DELIVERY')", name='role_check'),
     )
 
-
     def set_password(self,password):
         self.userpassword = generate_password_hash(password)
     
-
     def check_password(self,password):
         return check_password_hash(self.userpassword, password)
     
-    
     def get_id(self):
         return self.register_id 
-
-
-class Vendor(UserMixin, db.Model):
-    __tablename__ = "vendor"
-    id = db.Column(db.Integer, primary_key=True)
-    vendor_username = db.Column(db.String(30), db.ForeignKey('vendor_profile.v_username'))
+    
+    def __rep__(self):
+        return f"<Registered User : {self.username} > " #remove this
+'''
 
 class Menu(UserMixin, db.Model):
-    __tablename__="menu"
+    _tablename_="menu"
     vendor_id = db.Column(db.Integer,db.ForeignKey('vendor.id'), nullable=False)    
     item_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     discount=db.Column(db.Integer, default=0)
     price = db.Column(db.Numeric(8,2),nullable=False)
-    category = db.Column(db.String(20),nullable=False)
+    cuisine = db.Column(db.String(35),nullable=True) # Indian, Chinese, American etc
+    category = db.Column(db.String(35),nullable=True) # Rice, Noodles, BBQ, Vegetables, Roti, Gravy, FastFood
+    Type = db.Column(db.String(35),nullable=True) # Burger, Biryani, Ramen, etc
+    prep_time = db.Column(db.Integer,nullable=False) # Minutes
+    rating = db.Column(db.Integer,nullable=False) # 1-5
+    
+    
+@login.user_loader  #registers this fun with flask-login #####################################################
+def load_user(id):
+    try:
+        prefix, user_id = id.split('_')#since id in the parameter is a sring we first have to convert it 
+        
+        if prefix == 'cust':
+            return db.session.get(Customer, int(user_id))
+        elif prefix == 'vend':
+            return db.session.get(Vendor, int(user_id))
+        elif prefix == 'deliv':
+            return db.session.get(Delivery_Person, int(user_id))
+                
+    except (ValueError, AttributeError):
+        pass
+    return None
+
+class Locations(UserMixin, db.Model):
+    __tablename__ = "location"
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Numeric(15,10), nullable=False) 
+    longitude = db.Column(db.Numeric(15,10), nullable=False)
+    full_address = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    street = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=True)
+    osm_id = db.Column(db.String(75), nullable=False)
+    osm_type = db.Column(db.String(50), nullable=False)
+    data_source = db.Column(db.String(50), nullable=False)
+
+class Customer(UserMixin, db.Model):
+    __tablename__ = "customer"
+    id = db.Column(db.Integer, primary_key=True)
+    customer_location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    name = db.Column(db.String(50), nullable=False)
+
+    username = db.Column(db.String(30), unique=True)
+    user_phone = db.Column(db.String(11), nullable=False)
+    user_email = db.Column(db.String(30), nullable=False)
+    user_reg_date=db.Column(db.Date, nullable=False, default=date.today)
+    userpassword = db.Column(db.String(255), nullable=False)
+
+    def set_password(self,password):
+        self.userpassword = generate_password_hash(password)
+    
+    def check_password(self,password):
+        return check_password_hash(self.userpassword, password)
+      
+    def get_id(self):
+        return f"cust_{self.id}"
+    
+    def __rep__(self):
+        return f"<Customer : {self.username} > " #remove this
+
+class Vendor(UserMixin, db.Model):
+    __tablename__ = "vendor"
+    id = db.Column(db.Integer, primary_key=True)
+    vendor_location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    name = db.Column(db.String(50), nullable=False)
+    working_hours_start = db.Column(db.Time, nullable=False)
+    working_hours_end = db.Column(db.Time, nullable= False)
+
+    v_username = db.Column(db.String(30), unique=True)
+    v_phone = db.Column(db.String(11), nullable=False)
+    v_email = db.Column(db.String(30), nullable=False)
+    v_reg_date = db.Column(db.Date, nullable=False, default=date.today)  #How to add a date field in the form?
+    v_verified = db.Column(db.Boolean, nullable=False, default=False)
+    userpassword = db.Column(db.String(255), nullable=False)
+
+    def set_password(self,password):
+        self.userpassword = generate_password_hash(password)
+    
+    def check_password(self,password):
+        return check_password_hash(self.userpassword, password)
+      
+    def get_id(self):
+        return f"vend_{self.id}"
+
+    def pending_order_count(self):
+        1
+
+class Delivery_Person(UserMixin, db.Model):
+    __tablename__ = "delivery_person"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), nullable=True) # Person has an option to log in duty in the UI
+    salary = db.Column(db.Integer, nullable=True) #Save later in config File
+    #Salary is fixed per hour
+    last_paid=db.Column(db.Date, default=date.today)
+    worked_hours = db.Column(db.Integer, default=0) #worked_hours since last paid, to be reset and paid every week. Automated
+    current_location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=True)
+    wallet = db.Column(db.Numeric(8,2), default=12000) # he will be given an allowance to deal with orders. which is a minimum 12k when the allowance reaches 20k it has to be returned and dealt by admin
+    
+    d_username= db.Column(db.String(30), unique=True)
+    d_phone = db.Column(db.String(11), nullable=False)
+    d_email = db.Column(db.String(30), nullable=False)
+    d_reg_date = db.Column(db.Date, nullable=False, default=date.today)
+    v_verified = db.Column(db.Boolean, nullable=False, default=False)
+    userpassword = db.Column(db.String(255), nullable=False)
+
+    def set_password(self,password):
+        self.userpassword = generate_password_hash(password)
+    
+    def check_password(self,password):
+        return check_password_hash(self.userpassword, password)
+    
+    def get_id(self):
+        return f"deliv_{self.id}"
+
+    __table_args__ = (
+        db.CheckConstraint("status IN ('ON_DUTY','OFF_DUTY','IN_JOB')", name='status_check'),
+    )
     
  class Order(UserMixin,db.Model):
     __tablename__='order'
@@ -127,6 +180,7 @@ def load_user(id):
     return db.session.get(Registered_Users,int(id))  #since id in the parameter is a sring we first have to convert it 
 
 #NOTE: change the Primary key of all profile classes from username to id later on and cascade the changes. Much better I think.
+
 
 
 
